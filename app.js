18dfc8fef62666eb4fc982071f64510dc70c0eb7
CharlesThomaz <charlesthomaz87@gmail.com>
2026-09-07 16:19:17 -0300

    adiciona inicialização do Firebase e redirecionamento de autenticação

---
+++ app.js
@@ -0,0 +1,10 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-auth.js";
+import { firebaseConfig } from "./firebase-config.js";
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+if (auth) {
+  window.location.replace("index.html");
+}
